0dc92f780264bfb556d04439d092a876e3c941a1
JOHNFLEURIMOND <johncfleurimond@gmail.com>
2019-11-17 22:11:23 -0500

    make it better

--- index.js
+++ index.js
@@ -71,14 +71,14 @@ app.post("/api/form", (req, res) => {
 });
 
 app.use(bodyParser.json()); // get information from html forms
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV == 'production') {
   // Exprees will serve up production assets
-  app.use(express.static("/build/index.html"));
+  app.use(express.static('build'));
 
   // Express serve up index.html file if it doesn't recognize route
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve("/build/index.html"));
+  const path = require('path');
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'build/index.html'));
   });
 }
 
