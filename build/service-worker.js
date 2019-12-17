02e098026019b6cca9a7bf829cc4b00ae071425f
JOHNFLEURIMOND <johncfleurimond@gmail.com>
2019-12-17 18:13:54 -0500

    GH PAGES

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.37c100583091416e450e4cfc3e71b199.js"
+  "/JOHNFLEURIMOND/MERNStackForm/precache-manifest.384df36af8aa4cebdba3e0f67a06a960.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/JOHNFLEURIMOND/MERNStackForm/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
